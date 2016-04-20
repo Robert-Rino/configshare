37bd90fcf9438209712b78b29800fca804ad30ae
Soumya Ray <soumya.ray@gmail.com>
2016-04-20 14:37:06 +0800

    Move and modify route for getting account owned projects to account routes

--- db/seeds/accounts_projects_configurations.rb
+++ db/seeds/accounts_projects_configurations.rb
@@ -3,7 +3,7 @@ acc1 = Account.new(username: 'soumya.ray', email: 'sray@nthu.edu.tw')
 acc1.password = 'mypassword'
 acc1.save
 
-proj1 = acc.add_owned_project(name: 'Soumya Project')
+proj1 = acc1.add_owned_project(name: 'Soumya Project')
 proj1.repo_url = 'http://github.com/soumya/project.git'
 proj1.save
 doc11 = proj1.add_configuration(filename: 'config_env.rb')
@@ -14,7 +14,7 @@ doc12 = proj1.add_configuration(filename: 'environments.ini')
 doc12.document = '---'
 doc12.save
 
-proj2 = acc.add_owned_project(name: 'Config Project')
+proj2 = acc1.add_owned_project(name: 'Config Project')
 doc21 = proj2.add_configuration(filename: 'credentials.json')
 doc21.document = 'username: password'
 doc21.save
@@ -22,3 +22,8 @@ doc21.save
 acc2 = Account.new(username: 'lee123', email: 'lee@nthu.edu.tw')
 acc2.password = 'randompassword'
 acc2.save
+
+proj21 = acc2.add_owned_project(name: 'Lee\'s Project')
+proj22 = acc2.add_owned_project(name: 'Lee\'s Solo Project')
+acc1.add_project(proj21)
+acc2.add_project(proj2)
