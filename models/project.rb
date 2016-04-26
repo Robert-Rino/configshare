9f3527ded0f6f43597aadb50d17d897b7aa75c46
omarsar <ellfae@gmail.com>
2016-04-27 00:47:05 +0800

    there is a : symbol missing

--- models/project.rb
+++ models/project.rb
@@ -11,7 +11,7 @@ class Project < Sequel::Model
   one_to_many :configurations
   many_to_one :owner, class: :Account
   many_to_many :contributors,
-               class: Account, join_table: :accounts_projects,
+               class: :Account, join_table: :accounts_projects,
                left_key: :project_id, right_key: :contributor_id
 
   plugin :association_dependencies, configurations: :destroy
